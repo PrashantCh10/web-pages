ddd4ad65d3a6a95970d3c00c4d78c4d7ab102da2
PrashantCh10 <70701112+PrashantCh10@users.noreply.github.com>
2020-12-20 13:34:14 +0530

    Add files via upload

---
+++ src/emojipedia.js
@@ -0,0 +1,88 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "😅",
+    name: "Smiling Face With Open Mouth & Cold Sweat",
+    meaning:
+      "Sweaty smile. Relief that a tight situation went well or was coped with positively, e.g. an important appointment. A difficult event is imminent and you are already nervous."
+  },
+  {
+    id: 5,
+    emoji: "😌",
+    name: "Relieved Face",
+    meaning:
+      "Everything went well! Relieved smiley face. Is happy that something unpleasant is over without having caused any harm. Relaxed, thankful and free of worries."
+  },
+  {
+    id: 6,
+    emoji: "😋",
+    name: " Face Savouring Delicious Food",
+    meaning:
+      "Bon appetit! Smiley is licking the corner of its mouth with its tongue. Is hungry, just cooked something very delicious or eaten a whole dish. Often interpreted as a grimace, with tongue outstretched, and used after a funny message or when fooling around."
+  },
+  {
+    id: 7,
+    emoji: "😛",
+    name: " Face With Stuck-Out Tongue",
+    meaning:
+      "Horseplay with friends, weakening an ironic remark and making sure that the other does not take seriously what has been said. Making fun of others: I knew you'd screw it up."
+  },
+  {
+    id: 8,
+    emoji: "🤩",
+    name: " Smirking Face With Starry Eyes",
+    meaning:
+      "Is very excited about something and already full of anticipation. Overwhelmed and speechless after meeting someone you like a lot. Fascinated by the glory and the world of the stars."
+  },
+  {
+    id: 9,
+    emoji: "😒",
+    name: " Unamused Face",
+    meaning:
+      "The grumpy, sullen gaze expresses dissatisfaction. Is not enthusiastic about a thing and shows that. Expression of disinterest and disapproval."
+  },
+  {
+    id: 10,
+    emoji: "🥺",
+    name: " Face with begging eyes",
+    meaning:
+      "The big eyes are shining, devoted facial expression. The emoji is entreatingly begging for something: wants to borrow some money or the car. If you cannot get ahead by asking for something, puppy dog eyes are the last resort to get what you want."
+  },
+  {
+    id: 11,
+    emoji: "🥴",
+    name: " Face with unequal eyes and wavy mouth",
+    meaning:
+      "Eyes half open, half closed, slanted mouth, flushed cheeks. The emoji has a confused facial expression: due to tiredness or the currently confused emotional world. Also known as drunken face."
+  },
+  {
+    id: 12,
+    emoji: "😧",
+    name: " Anguished Face",
+    meaning:
+      "Something unexpected happened! Face with raised eyebrows, open eyes and open mouth. A mixture of shock and disappointment. Represents fear, frustration, horror and unexpected, negative surprises."
+  }
+];
+
+export default emojipedia;
